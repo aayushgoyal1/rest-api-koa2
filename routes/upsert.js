bfeeeb3b7e8f6dba3a597e4e36a41180a23854a4
Aayush Goyal <goyalaayush@yahoo.com>
2017-06-29 09:03:51 -0500

    Update upsert.js

--- routes/upsert.js
+++ routes/upsert.js
@@ -1,1 +1,19 @@
 
+/**
+ * dependencies
+ */
+
+import { isEmail } from 'validator'
+
+import Account from '../model'
+
+/**
+ * definitions
+ */
+
+export default async function upsert (ctx) {
+  if (!isEmail(ctx.request.body.email)) ctx.throw('Invalid email', 400)
+  const { created, data } = await Account.upsert(ctx.request.body)
+  ctx.status = created ? 201 : 200
+  ctx.body = { data }
+}
