fa220c6ace869b0412934eae58ec2250be1f0cad
Aayush Goyal <goyalaayush@yahoo.com>
2017-06-29 09:04:24 -0500

    Update tags.js

--- routes/tags.js
+++ routes/tags.js
@@ -1,1 +1,16 @@
 
+/**
+ * dependencies
+ */
+
+import Account from '../model'
+
+/**
+ * definitions
+ */
+
+export default async function findTagsByEmail (ctx, email) {
+  const account = await Account.findOne({ email })
+  if (!account) ctx.throw('Account not found', 404)
+  ctx.body = { data: account.tags }
+}
